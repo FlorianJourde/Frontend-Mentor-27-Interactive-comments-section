7b75c2a99c8c463c270a5f1bb532af013e1ca1aa
FlorianJourde <jourdeflorian@gmail.com>
2025-10-25 01:55:41 +0200

    build: dockerize app

--- lib/db.tsx
+++ lib/db.tsx
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise'
 
 const db = mysql.createPool({
   connectionLimit: 10,
-  host: 'srv904.hstgr.io',
-  user: 'u718790758_webask_user',
-  password: 'JUa91Q@y#9',
-  database: 'u718790758_webask_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 export default db;
