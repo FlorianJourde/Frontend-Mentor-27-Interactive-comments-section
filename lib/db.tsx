1c2acda0b31009fbe55f2c3c9a9832ad6b4caf13
FlorianJourde <jourdeflorian@gmail.com>
2025-10-25 02:23:46 +0200

    fix: resolve database connection

--- lib/db.tsx
+++ lib/db.tsx
@@ -1,12 +1,5 @@
 import mysql from 'mysql2/promise'
 
-console.log('Environment check:', {
-  DB_HOST: process.env.DB_HOST || 'MISSING',
-  DB_USER: process.env.DB_USER || 'MISSING',
-  DB_NAME: process.env.DB_NAME || 'MISSING',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'MISSING'
-})
-
 const db = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
